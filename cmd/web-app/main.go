c013b7b9b9c01b67d9b5f3d9c2896a3b65798808
Paul Watts <paul.watts@me.com>
2026-04-23 15:09:17 +1000

    serve web page

--- cmd/web-app/main.go
+++ cmd/web-app/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func main() {
+	http.HandleFunc("/", ShowHomePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ShowHomePage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("internal/views/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
